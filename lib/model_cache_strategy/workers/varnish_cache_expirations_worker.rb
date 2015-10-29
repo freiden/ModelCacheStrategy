88874fa8a6675ece4226b4430a32fc77755b0625
charles FD <charles.freitas-denke@vodeclic.com>
2015-10-29 12:19:44 +0100

    Update Varnish worker settings

--- lib/model_cache_strategy/workers/varnish_cache_expirations_worker.rb
+++ lib/model_cache_strategy/workers/varnish_cache_expirations_worker.rb
@@ -6,11 +6,16 @@ module ModelCacheStrategy
       include Sidekiq::Worker
 
       # sidekiq_options throttle: { threshold: 3, period: 1.second }, queue: :varnish_expiration
-      sidekiq_options throttle: -> { ModelCacheStrategy.configuration.varnish[:worker_throttling] },
-        queue: :varnish_expiration
+      sidekiq_options :queue => :varnish_expiration,  throttle: { #threshold: Proc.new { 3 }, period: Proc.new { 1.second } }
+        threshold: Proc.new { ModelCacheStrategy.configuration.varnish[:worker_throttling][:threshold] },
+        period:    Proc.new { ModelCacheStrategy.configuration.varnish[:worker_throttling][:period] }
+      }
+
+
+
 
       def perform(expiration_regex, callback_type = nil, index)
-        Rails.logger.debug "[VarnishCacheExpirationsWorker] Performing expiration (#{index}) for: #{expiration_regex}"
+        Rails.logger.debug "[#{DateTime.now.to_s}][VarnishCacheExpirationsWorker] Performing expiration (#{index}) for: #{expiration_regex}"
         adapter = ModelCacheStrategy::Adapters::Varnish.new
         adapter.expire_cache!(expiration_regex, callback_type: callback_type)
       end
